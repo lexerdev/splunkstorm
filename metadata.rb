c334340060cd23cb41977c7eaade8d448fbf6949
Aaron Wallis <aaron.wallis@gmail.com>
2012-10-07 11:00:02 +1100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "aaron.wallis@lexer.com.au"
 license          "Apache 2.0"
 description      "Installs/Configures Splunk Storm forwarders"
 long_description IO.read(File.join(File.dirname(__FILE__), 'readme.md'))
-version          "0.0.1"
+version          "0.0.2"
 %w{redhat centos fedora debian ubuntu}.each do |os|
   supports os
 end
